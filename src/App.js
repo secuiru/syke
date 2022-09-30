c204f8561f1a66fbdd83c7e60051263a56becba3
secuiru <vili.kauhanen@gmail.com>
2022-09-30 10:13:59 +0300

    final

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 
 function App() {
   const [age, setAge] = useState('20')
-  const [heart, setHeart] = useState('')
+  const [heart, setHeart] = useState(0)
+  const [heartlower,setlower] = useState(0)
 
   function laske(e){
     e.preventDefault()
-    setKilometri(mailit*1.609)
+    setHeart(220-age*0.65)
+    setlower(220-age*0.85)
   }
   return (
  
@@ -19,11 +21,11 @@ function App() {
       <form onSubmit={laske}>
       <div>
         <label>Age</label>
-        <input value={heart} onChange={e => setMailit(e.target.value)}/>
+        <input value={age} onChange={e => setAge(e.target.value)}/>
       </div>
       <div>
       <label>Heart rate limits</label>
-      <output>{heart}</output>
+      <output>{heartlower.toFixed(0)}-{heart.toFixed(0)}</output>
       </div>
       <button>Laske</button>
       </form>
